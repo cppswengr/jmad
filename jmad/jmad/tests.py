e3626bafdc5726e69a092f05c04958ee2d313c2b
H. Littles <hlittles@eVantageGroup.com>
2018-10-13 02:36:54 -0500

    Finishes first iteration of functional test

--- jmad/jmad/tests.py
+++ jmad/jmad/tests.py
@@ -86,13 +86,43 @@ class StudentTestCase(LiveServerTestCase):
             .find_elements_by_css_selector('.jmad-search-result')
         self.assertEqual(len(second_search_results), 2)
 
-        self.fail('Incomplete Test')
-
-
         # He clicks on a search result.
+        second_search_results[0].click()
+        self.fail('Incomplete Test')
 
         # The solo page has the title, artist and album for
         # this particular solo.
+        self.assertEqual(
+            self.browser.current_url,
+            '{}/solos/2/'.format(self.live_server_url)
+        )
+
+        self.assertEqual(
+            self.browser.find_element_by_css_selector(
+                '#jmad-artist').text,
+            'Cannonball Adderley'
+        )
+        self.assertEqual(
+            self.browser.find_element_by_css_selector(
+                '#jmad-track').text,
+            'All Blues'
+        )
+        self.assertEqual(
+            self.browser.find_element_by_css_selector(
+                '#jmad-album').text,
+            'Kind of Blue'
+        )
+        # He also sees the start time and end time of the solo.
+        self.assertEqual(
+            self.browser.find_element_by_css_selector(
+                '#jmad-start-time').text,
+            '2:06'
+        )
+        self.assertEqual(
+            self.browser.find_element_by_css_selector(
+                '#jmad-end-time').text,
+            '4:01'
+        )
 
         # He also sees the start time and end time of the
         # solo.
