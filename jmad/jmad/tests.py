aaf5f823e6bfe3e762955eec82b8ad2de53755a4
Brandon <Brandon@eVantageGroup.com>
2018-10-14 16:43:52 -0500

    Extends functional test for model and URL refactor

--- jmad/jmad/tests.py
+++ jmad/jmad/tests.py
@@ -36,6 +36,11 @@ class StudentTestCase(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def find_search_results(self):
+        return self.browser.find_elements_by_css_selector(
+            '.jmad-search-result a'
+        )
+
     def test_student_find_solos(self):
         """
         Test that a user can search for solos
@@ -95,42 +100,28 @@ class StudentTestCase(LiveServerTestCase):
 
         # The solo page has the title, artist and album for
         # this particular solo.
+        # On the solo page...
         self.assertEqual(
             self.browser.current_url,
-            '{}/solos/2/'.format(self.live_server_url)
+            self.live_server_url +
+            '/recordings/kind-of-blue/all-blues/cannonball-adderley/'
         )
-        self.assertEqual(
-            self.browser.find_element_by_css_selector(
-                '#jmad-track').text,
-            'All Blues'
-        )
+        # he sees the artist...
         self.assertEqual(
             self.browser.find_element_by_css_selector(
                 '#jmad-artist').text,
             'Cannonball Adderley'
         )
+        # the track title (with a count of solos)...
+        self.assertEqual(
+            self.browser.find_element_by_css_selector(
+                '#jmad-track').text,
+            'All Blues [2 solos]'
+        )
+        # and the album title (with track count) for this solo.
         self.assertEqual(
             self.browser.find_element_by_css_selector(
                 '#jmad-album').text,
-            'Kind of Blue'
+            'Kind of Blue [3 tracks]'
         )
-        # He also sees the start time and end time of the solo.
-        self.assertEqual(
-            self.browser.find_element_by_css_selector(
-                '#jmad-start-time').text,
-            '2:06'
-        )
-        self.assertEqual(
-            self.browser.find_element_by_css_selector(
-                '#jmad-end-time').text,
-            '4:01'
-        )
-        self.fail('Incomplete Test')
 
-        # He also sees the start time and end time of the
-        # solo.
-
-    def find_search_results(self):
-        return self.browser.find_elements_by_css_selector(
-            '.jmad-search-result a'
-        )
