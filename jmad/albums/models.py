0e051360559d305d48fd95a1112c0d4c2a8ff505
H. Littles <hlittles@eVantageGroup.com>
2018-10-16 07:14:45 -0500

    Adds string representation for Album and Track

--- jmad/albums/models.py
+++ jmad/albums/models.py
@@ -6,11 +6,17 @@ class Album(models.Model):
     artist = models.CharField(max_length=100)
     slug = models.SlugField()
 
+    def __str__(self):
+        return self.name
+
 
 class Track(models.Model):
     name = models.CharField(max_length=100)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     track_number = models.PositiveIntegerField(blank=True, null=True)
     slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
 
 
