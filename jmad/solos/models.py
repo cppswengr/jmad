4951acfd0a98094eb1dce49d8d2da9c9a0bf1321
Brandon <Brandon@eVantageGroup.com>
2018-10-19 10:55:30 -0500

    working on mocking musicbrainz

--- jmad/solos/models.py
+++ jmad/solos/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from django.urls import reverse
 
+from django.utils.text import slugify
+
 import musicbrainzngs as mb
 
 from albums.models import Track
@@ -35,7 +37,40 @@ class Solo(models.Model):
 
     @classmethod
     def get_artist_tracks_from_musicbrainz(cls, artist):
-        return mb.search_artists(artist)
+        """
+        Create Album, Track, and Solo records for artists we find
+        in the MusicBrainz API
+        :param artist: an artist's name as a string to search for
+        :return: Queryset of Solos
+        """
+        search_results = mb.search_artists(artist)
+        best_result = search_results['artist-list'][0]
+        instrument = Solo. \
+            get_instrument_from_musicbrainz_tags(
+            best_result['tag-list']
+        )
+        for album_dict in mb.browse_releases(
+                best_result['id'],
+                includes=['recordings'])['release-list']:
+            album = Album.objects. \
+                create(name=album_dict['title'],
+                       artist=artist,
+                       slug=slugify(album_dict['title']))
+        for track_dict in album_dict['medium-\
+        list'][0]['track-list']:
+            track = Track.objects.create(
+                album=album,
+                name=track_dict['recording']['title'],
+                track_number=track_dict['position'],
+                slug=slugify(
+                    track_dict['recording']['title']))
+            Solo.objects.create(
+                track=track, artist=artist,
+                instrument=instrument,
+                slug=slugify(artist))
+
+        return Solo.objects.filter(artist=artist)
+        # return mb.search_artists(artist)
 
     class Meta:
         ordering = ['track', 'start_time']
